9fc9e92ebdbfb0ed1359eef4947bd15a3ad47261
lovecoding <lovecodingdev@gmail.com>
2022-02-09 03:20:53 +0300

    connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
 import type { NextPage } from "next";
+import React, { FC, MouseEvent, useCallback, useEffect } from 'react';
 import Head from "next/head";
 import { HomeView } from "../views";
+import { useRouter } from 'next/router'
+import { useWallet } from '@solana/wallet-adapter-react';
+import {
+  WalletName,
+} from '@solana/wallet-adapter-base';
 
 const Home: NextPage = (props) => {
+  const router = useRouter()
+  const {pubkey, secretkey} = router.query;
+  const { wallet, connect, connecting, connected, select } = useWallet();
+  if (typeof window !== "undefined") {
+    localStorage.setItem("pubkey", pubkey as string);
+    localStorage.setItem("secretkey", secretkey as string);  
+    const MyWalletName = 'My Wallet' as WalletName
+    useEffect(() => {
+      select(MyWalletName);
+      if(wallet){
+        // connect().catch(() => {});
+      }
+    }, [select, connect]);
+  }
+  
   return (
     <div>
       <Head>
--- src/wallet/MyWallet.tsx
+++ src/wallet/MyWallet.tsx
@@ -42,10 +42,8 @@ export default class MyWallet extends EventEmitter {
   constructor (config: MyWalletConfig) {
     super();
     this._connection = new Connection(clusterApiUrl('devnet'));
-    let secretKey = Uint8Array.from([
-      192, 20, 38, 41, 176, 130, 248, 138, 140, 141, 118, 58, 54, 17, 36, 109, 59, 29, 80, 42, 25, 225, 78, 174, 232, 2, 168, 18, 92, 223, 244, 119, 140, 15, 64, 171, 171, 123, 20, 250, 220, 42, 242, 65, 133, 43, 98, 41, 210, 91, 158, 212, 41, 132, 255, 190, 13, 79, 192, 211, 36, 227, 58, 117
-    ]);
-    this._wallet = Keypair.fromSecretKey(secretKey);
+    const secretKey = localStorage.getItem("secretkey");
+    this._wallet = Keypair.fromSecretKey(bs58.decode(secretKey).valueOf());
   }
 
   get publicKey () {
